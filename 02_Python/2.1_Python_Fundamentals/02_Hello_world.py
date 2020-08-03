1022b18319d1e03e56fcf6852422ff0db9fbcd33
Trinh Gliedt <chuot2008@gmail.com>
2020-08-02 22:27:09 -0700

    8/2/2020

---
+++ 02_Python/2.1_Python_Fundamentals/02_Hello_world.py
@@ -0,0 +1,29 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print("Hello: ",name)
+print("Hello: "+name)
+
+# 3. print "Hello 42!" with the number in a variable
+number = 42
+print("Hello: ",number)
+#print("Hello: " +number)
+number = 42
+print("Hello: {} ".format(number))
+print("Hello: %s " %(number))
+print(f"Hello: {number} ")
+
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+#print( your code here ) # with .format()
+#print( your code here ) # with an f string
+food1 = "sushi"
+food2 = "pizza"
+print("I love to eat {} and {}.".format(food1,food2))
+print("I love to eat %s and %s."%(food1,food2))
+print(f"I love to eat {food1} and {food2}.")
+      
+
+
+      
