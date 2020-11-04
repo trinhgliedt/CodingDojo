98052751fa5a0b4b5830fa50fd3818f3c12172ee
Trinh Gliedt <chuot2008@gmail.com>
2020-11-04 15:23:38 -0800

    Justified string

--- 01_Algorithm/test.js
+++ 01_Algorithm/test.js
@@ -1,54 +1,21 @@
-// https://www.cs.usfca.edu/~galles/visualization/BST.html
+// Justify the given Text based on the given width of each line
+// Last Updated: 27-02-2020
+// Given a string str and width of each line as L, the task is to justify the string such that each line of justified text is of length L with help of space (‘ ‘) and the last line should be left-justified.
 
-class BSTNode {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-};
+// Examples:
+// Input: str = “GeeksforGeek is the best computer science portal for geeks.”, L = 16
+// Output:
+// GeeksforGeek__is
+// the_________best
+// computer_science
+// portal_______for
+// geeks.__________
 
-BST.insert(new BSTNode(10));
-
-class BST {
-    constructor() {
-        this.root = null;
-    }
-
-    isEmpty(){
-        return this.root === null;
-    };
-
-    // recursive
-    insert(node, tree){
-        if(tree === undefined){
-            tree = this.root;
-        };
-
-        // tree is null, which means the tree is empty (move root)
-
-        // find leaf node and add
-
-        // insert to left
-
-        // insert to the right return this.insert(node, tree.left);
-    };
-
-    getLargestFromSubtree(tree){
-        // if no tree, tree is root
-        if(tree === undefined){
-            tree = this.root;
-        }
-
-        // ...
-    }
-
-    getSmallestFromSubtree(tree){
-        // if no tree, tree is root
-        if(tree === undefined){
-            tree = this.root;
-        }
-
-        // ...
-    }
-};
+// Input: str = “The quick brown fox jumps over the lazy dog.”, L = 11
+// Output:
+// The___quick
+// brown___fox
+// jumped_over
+// the____lazy
+// dogs.______
+// The symbol _ denotes a space.
