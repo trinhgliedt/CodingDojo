d9737f0cca27fb3487c4dde5b2aac7df49897fa1
Trinh Gliedt <chuot2008@gmail.com>
2021-01-22 10:28:31 -0800

    1/22/21

--- 04_Mern/10_Mern_Portfolio/portfolio/client/src/components/Certificates.js
+++ 04_Mern/10_Mern_Portfolio/portfolio/client/src/components/Certificates.js
@@ -1,20 +1,57 @@
 
 
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
 import "../styles/CertificatesStyles.css";
 import dojoCert from "../images/cert_Dojo.JPG";
 import cpaCert from "../images/cert_CPA.jpg";
+import awsCert from "../images/cert_AWS.JPG";
 
 function Certificates() {
 
   return (
-    <div className="certContainer ml-4 ml-3 mt-2 pl-5 pt-4">
-    <img id="image1" 
-    src={cpaCert} alt="image1"></img>
-    <img id="image2" 
-    src={dojoCert} alt="image2"></img>
+    <>
+    <Carousel>
+      <Carousel.Item 
+        interval={7000}
+        className="">
+        <img
+          width="85%"
+          className="certImg d-block"
+          src={awsCert}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item 
+        interval={7000}
+        className="">
+        <img
+          width="85%"
+          className="certImg d-block"
+          src={dojoCert}
+          alt="Third slide"
+        />
 
-    </div>
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item 
+        interval={7000}
+        className="">
+        <img
+          width="85%"
+          className="certImg d-block"
+          src={cpaCert}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </>
   );
 }
 export default Certificates;
--- 06_Post_Dojo_Projects/01_Expense_approval/client/src/App.js
+++ 06_Post_Dojo_Projects/01_Expense_approval/client/src/App.js
@@ -1,34 +1,39 @@
 import React from "react";
 import "./App.css";
+import "./chart1.css";
 
-import { Link, Redirect, Router } from "@reach/router";
+// import { Link, Redirect, Router } from "@reach/router";
 
-import NotFound from "./views/NotFound";
-import NewCity from "./views/NewCity";
-import Cities from "./views/Cities";
-import SingleCity from "./views/SingleCity";
-import EditCity from "./views/EditCity";
-import NodeComponent from "./views/Graph";
+// import NotFound from "./views/NotFound";
+// import NewCity from "./views/NewCity";
+// import Cities from "./views/Cities";
+// import SingleCity from "./views/SingleCity";
+// import EditCity from "./views/EditCity";
+// import NodeComponent from "./views/Graph";
 import Chart from "./views/Chart";
 
 function App() {
   return (
-    <div className="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top justify-content-center">
+    <div className="App">
+      <div className="container">
+        <Chart />
+      </div>
+      {/* <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top justify-content-center"> */}
         
-        <h1 class="navbar-brand">Cool City, Bro</h1>
-        <div class="navbar-nav justify-content-between">
-          <Link to="/cities">
+        {/* <h1 class="navbar-brand">Cool City, Bro</h1> */}
+        {/* <div class="navbar-nav justify-content-between"> */}
+          {/* <Link to="/cities">
             {/* The span is only needed because className doesn't work directly on a component tag */}
-            <span className="btn btn-sm btn-outline-primary mr-1">Cities</span>
-          </Link>
-          <Link to="/cities/new">
+            {/* <span className="btn btn-sm btn-outline-primary mr-1">Cities</span>
+          </Link> */} 
+          {/* <Link to="/cities/new">
             <span className="btn btn-sm btn-outline-primary">New City</span>
-          </Link>
-        </div>
-      </nav>
+          </Link> */}
+        {/* </div> */}
+      {/* </nav> */}
 
-      <div style={{ marginTop: "10%" }}>
+      {/* <div style={{ marginTop: "10%" }}>
+        <Chart/>
         <Router>
           <Chart path="/chart"/>
           <NodeComponent path="/graph" />
@@ -39,7 +44,7 @@ function App() {
           <EditCity path="/cities/:id/edit" />
           <NotFound default />
         </Router>
-      </div>
+      </div> */}
     </div>
   );
 }
