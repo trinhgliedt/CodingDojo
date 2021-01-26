07b1d116a9de6fdc10a3ab2d57de805c331e126a
Trinh Gliedt <chuot2008@gmail.com>
2021-01-26 09:56:14 -0800

    react router/custom outs/footer component

--- 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/App.js
+++ 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./default.scss";
-import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
 
+//layouts
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout";
+
+//pages
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
