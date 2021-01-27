ccc8146951684e8842488cfcf903f4a166584d60
Trinh Gliedt <chuot2008@gmail.com>
2021-01-26 23:28:39 -0800

    email, password auth using Firebase

--- 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/App.js
+++ 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/App.js
@@ -1,39 +1,92 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import "./default.scss";
-import Homepage from "./pages/Homepage";
-import Registration from "./pages/Registration";
-
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { auth, handleUserProfile } from "./firebase/utils";
 //layouts
 import MainLayout from "./layouts/MainLayout";
 import HomepageLayout from "./layouts/HomepageLayout";
 
 //pages
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <HomepageLayout>
-              <Homepage />
-            </HomepageLayout>
-          )}
-        />
+import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
 
-        <Route
-          path="/registration"
-          render={() => (
-            <MainLayout>
-              <Registration />
-            </MainLayout>
-          )}
-        />
-      </Switch>
-    </div>
-  );
+import "./default.scss";
+const initialState = {
+  currentUser: null,
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+  }
+  authListener = null;
+  componentDidMount() {
+    this.authListener = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+        });
+      }
+      this.setState({
+        ...initialState,
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.authListener();
+  }
+  render() {
+    const { currentUser } = this.state;
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <HomepageLayout currentUser={currentUser}>
+                <Homepage />
+              </HomepageLayout>
+            )}
+          />
+
+          <Route
+            path="/registration"
+            render={() =>
+              currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <MainLayout currentUser={currentUser}>
+                  <Registration />
+                </MainLayout>
+              )
+            }
+          />
+          <Route
+            path="/login"
+            render={() =>
+              currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <MainLayout currentUser={currentUser}>
+                  <Login />
+                </MainLayout>
+              )
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/pages/Registration/index.js
+++ 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/pages/Registration/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Signup from "../../components/Signup";
 import "./styles.scss";
 
 class Registration extends Component {
   render() {
     return (
       <div>
-        <h1>Registration page</h1>
+        <Signup />
       </div>
     );
   }
