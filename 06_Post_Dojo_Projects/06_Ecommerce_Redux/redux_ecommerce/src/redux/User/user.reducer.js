6da65e6171d74a8c86c430b64a3514f8d03d0e40
Trinh Gliedt <chuot2008@gmail.com>
2021-01-29 00:00:47 -0800

    refactoring using redux hooks useSelector and useDispatch

--- 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/redux/User/user.reducer.js
+++ 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/redux/User/user.reducer.js
@@ -2,6 +2,8 @@ import userTypes from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
+  signInSuccess: false,
+  signUpError: [],
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +12,21 @@ const userReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         currentUser: action.payload,
+      };
+    case userTypes.SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        signInSuccess: action.payload,
+      };
+    case userTypes.SIGN_UP_ERROR:
+      return {
+        ...state,
+        signUpError: action.payload,
+      };
+    case userTypes.SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        signUpSuccess: action.payload,
       };
     default:
       return state;
