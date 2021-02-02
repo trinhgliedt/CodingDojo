3169e1e1e933a279c90f7f670a67d078afd52641
Trinh Gliedt <chuot2008@gmail.com>
2021-02-02 10:37:14 -0800

    2/2/21

--- 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/components/Checkout/index.js
+++ 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/components/Checkout/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectCartItems, selectCartTotal } from './../../redux/Cart/cart.selectors';
-import { createStructuredSelector } from 'reselect';
-import './styles.scss';
-import Button from './../forms/Button';
-import Item from './Item';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "./../../redux/Cart/cart.selectors";
+import { createStructuredSelector } from "reselect";
+import "./styles.scss";
+import Button from "./../forms/Button";
+import Item from "./Item";
 
 const mapState = createStructuredSelector({
   cartItems: selectCartItems,
-  total: selectCartTotal
+  total: selectCartTotal,
 });
 
-const Checkout = ({ }) => {
+const Checkout = ({}) => {
   const history = useHistory();
   const { cartItems, total } = useSelector(mapState);
 
-  const errMsg = 'You have no items in your cart.';
+  const errMsg = "You have no items in your cart.";
 
   return (
     <div className="checkout">
-      <h1>
-        Checkout
-      </h1>
+      <h1>Checkout</h1>
 
       <div className="cart">
         {cartItems.length > 0 ? (
@@ -30,24 +31,19 @@ const Checkout = ({ }) => {
             <tbody>
               <tr>
                 <td>
-                  <table className="checkoutHeader" border="0" cellPadding="10" cellSpacing="0">
+                  <table
+                    className="checkoutHeader"
+                    border="0"
+                    cellPadding="10"
+                    cellSpacing="0"
+                  >
                     <tbody>
                       <tr>
-                        <th>
-                          Product
-                        </th>
-                        <th>
-                          Description
-                        </th>
-                        <th>
-                          Quantity
-                        </th>
-                        <th>
-                          Price
-                        </th>
-                        <th>
-                          Remove
-                        </th>
+                        <th>Product</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Remove</th>
                       </tr>
                     </tbody>
                   </table>
@@ -80,9 +76,7 @@ const Checkout = ({ }) => {
                             <tbody>
                               <tr>
                                 <td>
-                                <h3>
-                                  Total: £{total}
-                                </h3>
+                                  <h3>Total: ${total}</h3>
                                 </td>
                               </tr>
                             </tbody>
@@ -100,7 +94,9 @@ const Checkout = ({ }) => {
                                   </Button>
                                 </td>
                                 <td>
-                                  <Button onClick={() => history.push('/payment')}>
+                                  <Button
+                                    onClick={() => history.push("/payment")}
+                                  >
                                     Checkout
                                   </Button>
                                 </td>
@@ -116,10 +112,8 @@ const Checkout = ({ }) => {
             </tbody>
           </table>
         ) : (
-            <p>
-              {errMsg}
-            </p>
-          )}
+          <p>{errMsg}</p>
+        )}
       </div>
     </div>
   );
--- 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/components/Footer/index.js
+++ 06_Post_Dojo_Projects/06_Ecommerce_Redux/redux_ecommerce/src/components/Footer/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
-const Footer = props => {
+const Footer = (props) => {
   return (
     <footer className="footer">
-      <div className="wrap">
-        © SimpleTut 2020
-      </div>
+      <div className="wrap">© ForeverYou 2021</div>
     </footer>
   );
-}
+};
 
 export default Footer;
